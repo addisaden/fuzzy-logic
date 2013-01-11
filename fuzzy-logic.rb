58c0669912347405cc2492f984d815006b1d9302
addisaden <ae@writedown.eu>
2013-01-11 10:00:11 +0100

    just formatting ...

--- fuzzy-logic.rb
+++ fuzzy-logic.rb
@@ -64,11 +64,11 @@ module FuzzyLogic
       range = range.abs
       return Set.new(1) { |n|
         o = 0.0
-	      if n == center then
-	        o = 1.0
-	      elsif n.between?((center-range/2.0), (center+range/2.0)) then
-	        o = 1.0 - [1.0, (n - center).abs.to_f/(range/2.0)].min
-	      end
+        if n == center then
+          o = 1.0
+        elsif n.between?((center-range/2.0), (center+range/2.0)) then
+          o = 1.0 - [1.0, (n - center).abs.to_f/(range/2.0)].min
+        end
 	      o
       }
     end
